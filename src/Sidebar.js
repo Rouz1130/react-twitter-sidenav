40a2b96daaca41339cdb14ee107ac732ccf9109a
Rouz Majlessi <Majlessi.Rouzbeh@mayo.edu>
2022-02-08 14:41:34 -0800

    add sidbar options

---
+++ src/Sidebar.js
@@ -0,0 +1,18 @@
+import "./sidebar.css";
+import SidebarLink from "./SidebarLink";
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+            <SidebarLink text="Home" />
+            <SidebarLink text="Explore" />
+            <SidebarLink text="Notifications" />
+            <SidebarLink text="Messages" />
+            <SidebarLink text="Bookmarks" />
+            <SidebarLink text="Lists" />
+            <SidebarLink text="Profile" />
+            <SidebarLink text="More" />
+        </div>
+    );
+}
+export default Sidebar;
