c03a85f50faaa55338c911c2ba815413b5e50e7d
Rouz Majlessi <Majlessi.Rouzbeh@mayo.edu>
2022-02-08 15:07:52 -0800

    Fix typos and display sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,18 +1,19 @@
 import "./sidebar.css";
 import SidebarLink from "./SidebarLink";
 
-function Sidebar() {
-    return(
-        <div className="sidebar">
-            <SidebarLink text="Home" />
-            <SidebarLink text="Explore" />
-            <SidebarLink text="Notifications" />
-            <SidebarLink text="Messages" />
-            <SidebarLink text="Bookmarks" />
-            <SidebarLink text="Lists" />
-            <SidebarLink text="Profile" />
-            <SidebarLink text="More" />
-        </div>
-    );
+function Sidebar(){
+  return(
+    <div className="sidebar">
+        <SidebarLink text="Home" />
+        <SidebarLink text="Explore" />
+        <SidebarLink text="Notifications" />
+        <SidebarLink text="Messages" />
+        <SidebarLink text="Bookmarks" />
+        <SidebarLink text="Lists" />
+        <SidebarLink text="Profile" />
+        <SidebarLink text="More" />
+    </div>
+  );
 }
+
 export default Sidebar;
---
+++ src/SidebarLink.js
@@ -0,0 +1,10 @@
+import "./sidebarLink.css";
+
+function SidebarLink({ text }) {
+  return(
+    <div className="link" >
+        <h2>{text}</h2>
+    </div>
+  );
+}
+export default SidebarLink;
