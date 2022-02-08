195418bdcb59f8166b25cf3df6a524aec4d01cbb
Rouz Majlessi <Majlessi.Rouzbeh@mayo.edu>
2022-02-08 15:33:37 -0800

    add twitter button to sidbar component and active state

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,12 +11,12 @@ import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import Search from "@material-ui/icons/Search";
-
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarLink text="Home" Icon={HomeIcon} />
+            <SidebarLink text="Home" active={true} Icon={HomeIcon} />
             <SidebarLink text="Explore" Icon={SearchIcon} />
             <SidebarLink text="Notifications" Icon={NotificationsNoneIcon} />
             <SidebarLink text="Messages" Icon={MailOutlineIcon} />
@@ -24,7 +24,9 @@ function Sidebar() {
             <SidebarLink text="Lists" Icon={ListAltIcon} />
             <SidebarLink text="Profile" Icon={PermIdentityIcon} />
             <SidebarLink text="More" Icon={MoreHorizIcon} />
-
+            <Button id="tweet">
+                Tweet
+            </Button>
         </div>
     );
 }
